144815bbb1aa9b1e7b090b5451061cf8d5f822a4
Fdvky1 <fdvkey@gmail.com>
2023-06-13 15:52:24 +0700

    remove user agent

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function download(url){
             "accept": "*/*",
             "accept-language": "en-US,en;q=0.9",
             "content-type": `multipart/form-data; boundary=${form.getBoundary()}`,
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
             "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": "\"Linux\"",
